0f7dc23c7b3fa3c9fa47d4709d90f4052276fff8
Nazario A. Ayala <nazario@niztech.com>
2021-10-30 15:35:27 -0700

    Added input for last node

--- trees/src/tree/TreeNode.js
+++ trees/src/tree/TreeNode.js
@@ -1,14 +1,24 @@
-const TreeNode = ({node, children, upsideDown = false}) => {
+const TreeNode = ({node, children, upsideDown = false, level = 0, isLastNode = false}) => {
   children = Array.isArray(children) ? children : [];
   const data = upsideDown && children.length ? children.reverse() : children;
+
+  const inputElement = isLastNode && (<div>
+    <label>Add item <span className="sr-only">under {node}</span>:
+      <input type="text"/>
+    </label>
+  </div>);
 
   return (
     <li className={upsideDown ? 'reverse title' : 'title'}>
       {!upsideDown && node}
+
+      {inputElement}
+
       {data &&
-      data.map((n, index) => (
+      data.map((n, index, array) => (
         <ol key={node} className="values indent" start={index + 1}>
-          <TreeNode node={n.node} children={n.children} upsideDown={upsideDown}/>
+          <TreeNode node={n.node} children={n.children} upsideDown={upsideDown} level={1 + level}
+                    isLastNode={array.length === index + 1}/>
         </ol>
       ))}
       {upsideDown && node}
