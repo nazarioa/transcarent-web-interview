c6fbd7038525be50ba665b3b13df61fafe53016b
Nazario A. Ayala <nazario@niztech.com>
2021-10-29 21:39:33 -0700

    Total guess, read this comment ..
    I started structing this as I would have with semantic HTML.
    
    I noticed that There was a lot of repetative nested code so I used the
    snippet labeled (Fig A) and pasted it within each nest, removing the
    `ol.values` where needed.
    
    I gave the HTML tags classnames that match the JSON object from step 2.
    Aside: I did the JSOn thing first becuase I am guessig at what the ask
    is for this section. That said, I feel like I am in the right track.
    
    Not sure what I am goung to do with CSS... still mulling. I'll leave it
    for the end. I suspect maybe sudo element, something like
    `content: '&nbsp;'` multiplied by level to get increments by 4.

--- trees/src/tree/index.js
+++ trees/src/tree/index.js
@@ -1,8 +1,9 @@
-import "./index.css";
+import './index.css';
 
-export default function Tree() {
+export default function Tree () {
   return (
     <div className="tree">
+      <h2>Original</h2>
       <p>root</p>
       <p>&nbsp;&nbsp;&nbsp;&nbsp;ant</p>
       <p>&nbsp;&nbsp;&nbsp;&nbsp;bear</p>
@@ -12,6 +13,42 @@ export default function Tree() {
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;elephant
       </p>
       <p>&nbsp;&nbsp;&nbsp;&nbsp;frog</p>
+
+      <hr/>
+
+      <ol className="values">
+        <li className="title">root
+          <ol className="values">
+            <li className="title">ant</li>
+            <li className="title">bear
+              <ol className="values">
+                <li className="title">cat</li>
+                <li className="title">dog</li>
+              </ol>
+            </li>
+            <li className="title">frog</li>
+          </ol>
+        </li>
+      </ol>
     </div>
   );
 }
+
+// Notes
+
+// FIG A.
+// <li className="title"> TITLE
+//   <ol className="values"> VALUES </ol> {/* Optional */}
+// </li>
+
+/*
+   0  <p>root</p>
+   4  <p>&nbsp;&nbsp;&nbsp;&nbsp;ant</p>
+   4  <p>&nbsp;&nbsp;&nbsp;&nbsp;bear</p>
+   8  <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;cat</p>
+   8  <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;dog</p>
+      <p>
+  12    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;elephant
+      </p>
+   4  <p>&nbsp;&nbsp;&nbsp;&nbsp;frog</p>
+ */
