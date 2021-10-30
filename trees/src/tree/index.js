819452930010457a946633ee439eeeb404cf02db
Nazario A. Ayala <nazario@niztech.com>
2021-10-29 22:48:04 -0700

    fixup! Step 3 - Showing my work

--- trees/src/tree/index.js
+++ trees/src/tree/index.js
@@ -24,7 +24,9 @@ export default function Tree () {
               <ol className="values">
                 <li className="title">cat</li>
                 <li className="title">dog
-                  <li className="title">elephant</li>
+                  <ol className="title">
+                    <li>elephant</li>
+                  </ol>
                 </li>
               </ol>
             </li>
