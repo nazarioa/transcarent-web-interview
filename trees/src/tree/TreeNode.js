0f73c874e91938924b87afb8efdcdec0d5b430ed
Nazario A. Ayala <nazario@niztech.com>
2021-10-29 23:37:25 -0700

    ... upside down (part 2)
    What I think you mean by upside down although the ::marker appears at
    the top of the li box, and my positioing tricks are not working.
    
    will look into this later.. I think I am over time anyway :P

--- trees/src/tree/TreeNode.js
+++ trees/src/tree/TreeNode.js
@@ -1,4 +1,18 @@
-const TreeNode = ({node, children}) => {
+const TreeNode = ({node, children, upsideDown = false}) => {
+  if(upsideDown) {
+    return (
+      <li className="title">
+        {children &&
+        children.reverse().map(({node, children}, index) => (
+          <ol key={node} className="values indent" start={index + 1}>
+            <TreeNode node={node} children={children} upsideDown={upsideDown}/>
+          </ol>
+        ))}
+        {node}
+      </li>
+    );
+  }
+
   return (
     <li className="title">
       {node}
